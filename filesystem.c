1269981df82f3e83b10b7e9d49563ca75ebb45c5
Cordisss <safinislam624@gmail.com>
2025-05-15 09:39:12 +0300

    Update filesystem.c

--- filesystem.c
+++ filesystem.c
@@ -101,25 +101,3 @@ void delete_file(const char* fs_filename, const char* target_filename) {
     }
     fclose(new_fs);
 }
-
-// Пример main
-int main() {
-    const char* fs_name = "fs.txt";
-    FILE* fs = open_fs(fs_name);
-    if (!fs) {
-        printf("Не удалось открыть или создать файл\n");
-        return 1;
-    }
-    fclose(fs);
-
-    printf("Содержимое файла file1.txt:\n");
-    view_file(fs_name, "file1.txt");
-
-    printf("\nУдаление file1.txt...\n");
-    delete_file(fs_name, "file1.txt");
-
-    printf("\nПовторная проверка file1.txt:\n");
-    view_file(fs_name, "file1.txt");
-
-    return 0;
-}
