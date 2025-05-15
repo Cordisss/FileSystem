8eea2fca7e24025c4808749542140334e8287e00
Cordisss <safinislam624@gmail.com>
2025-05-15 10:39:18 +0300

    Update filesystem.c

--- filesystem.c
+++ filesystem.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define MAX_FILE_SIZE 2048
 #define MAX_LINES 256
 
@@ -102,3 +101,45 @@ void delete_file(const char* fs_filename, const char* target_filename) {
     fclose(new_fs);
 }
 
+// Добавить новый файл
+void add_new_file(const char* fs_filename, const char* new_filename, const char* content_lines[], int content_count) {
+    FILE* fs = fopen(fs_filename, "a");
+    if (!fs) {
+        printf("Ошибка: не удалось открыть файловую систему\n");
+        return;
+    }
+
+    // Проверяем, существует ли уже файл с таким именем
+    FILE* check_fs = fopen(fs_filename, "r");
+    if (check_fs) {
+        char buffer[MAX_FILE_SIZE] = {0};
+        fread(buffer, 1, MAX_FILE_SIZE - 1, check_fs);
+        fclose(check_fs);
+
+        char* token = strtok(buffer, "\n");
+        while (token) {
+            if (strcmp(token, new_filename) == 0) {
+                printf("Ошибка: файл с таким именем уже существует\n");
+                fclose(fs);
+                return;
+            }
+            token = strtok(NULL, "\n");
+        }
+    }
+
+    fprintf(fs, "%s\n", new_filename);
+    for (int i = 0; i < content_count; i++) {
+        fprintf(fs, "%s\n", content_lines[i]);
+    }
+    fprintf(fs, "/end\n");
+    fclose(fs);
+}
+
+// Изменить содержимое файла
+void modify_file(const char* fs_filename, const char* target_filename, const char* new_content[], int new_count) {
+    // Сначала удаляем старый файл
+    delete_file(fs_filename, target_filename);
+    
+    // Затем добавляем его с новым содержимым
+    add_new_file(fs_filename, target_filename, new_content, new_count);
+}
